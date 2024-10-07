b88dbd219e41c3fbaca0f8850b1076bf3b90504e
mansh2000 <waliamanshu17@gmail.com>
2024-10-07 16:05:58 +0530

    added changes

--- src/services/email.service.js
+++ src/services/email.service.js
@@ -6,7 +6,6 @@ const { Otp } = require('../models');
 const ApiError = require('../utils/ApiError');
 
 const transport = nodemailer.createTransport(config.email.smtp);
-/* istanbul ignore next */
 if (config.env !== 'test') {
   transport
     .verify()
@@ -14,27 +13,15 @@ if (config.env !== 'test') {
     .catch(() => logger.warn('Unable to connect to email server. Make sure you have configured the SMTP options in .env'));
 }
 
-/**
- * Send an email
- * @param {string} to
- * @param {string} subject
- * @param {string} text
- * @returns {Promise}
- */
+
 const sendEmail = async (to, subject, text) => {
   const msg = { from: config.email.from, to, subject, text };
   await transport.sendMail(msg);
 };
 
-/**
- * Send reset password email
- * @param {string} to
- * @param {string} token
- * @returns {Promise}
- */
+
 const sendResetPasswordEmail = async (to, token) => {
   const subject = 'Reset password';
-  // replace this url with the link to the reset password page of your front-end app
   const code = Math.floor(1000 + Math.random() * 9000);
   const otpObject = {
     otp: code,
@@ -48,15 +35,9 @@ If you did not request any password resets, then ignore this email.`;
   await sendEmail(to, subject, text);
 };
 
-/**
- * Send verification email
- * @param {string} to
- * @param {string} token
- * @returns {Promise}
- */
+
 const sendVerificationEmail = async (to, token) => {
   const subject = 'Email Verification';
-  // replace this url with the link to the email verification page of your front-end app
   const verificationEmailUrl = `http://link-to-app/verify-email?token=${token}`;
   const code = Math.floor(1000 + Math.random() * 9000);
 
