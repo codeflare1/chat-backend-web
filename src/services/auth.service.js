806195872fbc54849fea7c01bf6a52b2305ea59e
mansh2000 <waliamanshu17@gmail.com>
2024-10-08 21:11:20 +0530

    added changes

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -25,7 +25,8 @@ const sendOtp = async (userBody) => {
 };
 
 const login = async (userBody) => {
-  const user =  await User.isPhoneNumberTaken(userBody.phoneNumber);
+  const user =  await User.findOne({phoneNumber: userBody.phoneNumber});
+  console.log(user);
   if (!user || !(await user.isPinMatch(userBody.pin))) {
     throw new ApiError(httpStatus.UNAUTHORIZED, 'Incorrect phoneNumber & pin');
   }
