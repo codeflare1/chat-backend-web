d9673b617864eba9f4fe75b3a22e9708a6a68e68
bhuvneshindoriya <bhuvneshindoriya0909@gmail.com>
2024-10-19 02:16:25 +0530

    done

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -120,6 +120,7 @@ const setPin = catchAsync(async (req, res) => {
 });
 
 const loginWithPin = catchAsync(async (req, res) => {
+  console.log(req.body)
   const user = await authService.loginWithPin(req);
   res.status(httpStatus.OK).send({ success: true, user });
 });
