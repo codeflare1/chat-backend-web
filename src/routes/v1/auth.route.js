bf109a12ad19e4bd897c22d3aa114cf691faf5f5
bhuvneshindoriya <bhuvneshindoriya0909@gmail.com>
2024-10-18 23:45:55 +0530

    done

--- src/routes/v1/auth.route.js
+++ src/routes/v1/auth.route.js
@@ -12,7 +12,7 @@ const router = express.Router();
 router.post('/register', auth(), upload.single('userProfileImage'), validate(authValidation.register), authController.register);
 router.post('/login', validate(authValidation.login), authController.login);
 router.post('/send-otp',validate(authValidation.sendOtp), authController.sendOtp);
-router.post('/verify-otp', validate(authValidation.verifyOtp), authController.verifyOtp);
+router.get('/verify-otp', validate(authValidation.verifyOtp), authController.verifyOtp);
 router.post('/set-pin', validate(authValidation.createPin), authController.setPin);
 router.post('/forgot-pin', validate(authValidation.forgotPin), authController.forgotPin);
 router.post('/loginWithPin', auth(), validate(authValidation.loginWithPin), authController.loginWithPin);
