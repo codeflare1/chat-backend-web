b8d64e014b8efd805690f5848a63c371fbc3edf4
bhuvneshindoriya <bhuvneshindoriya0909@gmail.com>
2024-10-27 18:11:17 +0530

    done

--- src/models/groupModel.js
+++ src/models/groupModel.js
@@ -4,6 +4,7 @@ const GroupSchema = new mongoose.Schema({
   groupId: { type: String, required: true, unique: true },
   groupName: { type: String, required: true },
   adminId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  image: { type: String, required: false },
   members: [
     {
       userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
